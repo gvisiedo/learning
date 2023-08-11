93e28452508330e5622959a3a0a4ad895247d3d3
VisiGoyo <79362138+gvisiedo@users.noreply.github.com>
2023-08-11 12:29:06 +0200

    Creados seleccion mascota jugador y Enemigo

--- js/script.js
+++ js/script.js
@@ -1,26 +1,68 @@
+let ataqueJugador 
+
 function iniciarJuego(){
     let botonMascotaJugador = document.getElementById('boton-mascota');
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador);
 
+    let botonFuego = document.getElementById('boton-fuego');
+    botonFuego.addEventListener('click', ataqueFuego)
+    let botonAgua = document.getElementById('boton-agua');
+    botonAgua.addEventListener('click', ataqueAgua)
+    let botonTierra = document.getElementById('boton-tierra');
+    botonTierra.addEventListener('click', ataqueTierra)
 }
+
 
 function seleccionarMascotaJugador(){
     let inputHipodoge = document.getElementById('hipodoge');
     let inputCapipepo = document.getElementById('capipepo');
     let inputRatigueya = document.getElementById('ratigueya');
+    let spanMascotaJugador = document.getElementById('mascota-jugador');
 
 
 
    if(inputHipodoge.checked){
-    alert('Seleccionaste a Hipodoge')
+    spanMascotaJugador.innerHTML ='Hipodoge';
     } else if(inputCapipepo.checked){
-    alert('Seleccionaste Capipepo')    
+        spanMascotaJugador.innerHTML ='Capipepo';    
     }else if(inputRatigueya.checked){
-        alert('Seleccionaste Ratigueya')
+        spanMascotaJugador.innerHTML ='Ratigueya';
     }else {
-        alert('Selecciona una mascota')
+        alert('Selecciona una mascota');
     }
 
+    seleccionarMascotaEnemigo()
+
+}
+function seleccionarMascotaEnemigo(){
+    let ataqueAleatorio = aleatorio(1,3);
+    let spanMascotaEnemigo = document.getElementById('mascota-enemigo');
+
+    if(ataqueAleatorio==1){
+        spanMascotaEnemigo.innerHTML = 'Hipodoge';
+
+    }else if(ataqueAleatorio==2){
+        spanMascotaEnemigo.innerHTML = 'Capipepo';
+    }else {
+        spanMascotaEnemigo.innerHTML = 'Ratigueya';
+    }
+}
+
+function ataqueFuego(){
+    ataqueJugador = 'Fuego'
+    alert(ataqueJugador)
+}
+function ataqueAgua(){
+    ataqueJugador = 'Agua'
+    alert(ataqueJugador)
+}
+function ataqueTierra(){
+    ataqueJugador = 'Tierra'
+    alert(ataqueJugador)
+}
+
+function aleatorio(min,max){
+    return Math.floor(Math.random()*(max - min + 1) + min);
 }
 
 window.addEventListener('load', iniciarJuego)
